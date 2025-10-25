9bc2ee6e6675f1a83fa31fa47d7daaef332c2b85
fhraannsa <fahirann6@gmail.com>
2025-10-25 21:05:04 +0700

    250441100096_FahiraAnnisa_UTS_asprakkakKhanif

---
+++ UTS/UTS_ALPRO1A_25-096_Fahira Annisa/uts.py
@@ -0,0 +1,35 @@
+motor_matic = 5000000 #Rp50.000.0o
+motor_trail = 10000000 #Rp100.000.00.
+motor_sprot = 7500000 #Rp75.000.00.
+
+asuransi = 1500000 #Rp15.000.00.
+penyewaan = 0 
+diskon = 0
+kupon = "AconkGG"
+
+
+while True:
+    if motor in penyewaan:
+     motor > 3 
+     motor = input("masukkan motor:", motor)
+    berlaku_kelipatan: 15
+    print("sewa lebih dari 3 hari:", berlaku_kelipatan)
+
+    if motor in penyewaan:
+       motor >  15000000 #150.000.00.
+       motor = input("masukkan motor:", motor)
+       diskon > 0.10
+       print("mendapatkan diskon:", diskon)
+
+    elif motor in penyewaan:
+       motor > 0.5
+       motor = input("masukkan motor:", motor)
+       diskon > 0.5
+       print("diskon tambahan:", diskon)
+       break
+
+
+    
+
+
+
