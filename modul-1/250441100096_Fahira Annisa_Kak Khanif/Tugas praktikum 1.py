33d4ee6827d4cf701c1dc30f665f3f26e735608d
fhraannsa <fahirann6@gmail.com>
2025-10-25 20:42:41 +0700

    250441100096

---
+++ modul-1/250441100096_Fahira Annisa_Kak Khanif/Tugas praktikum 1.py
@@ -0,0 +1,14 @@
+buku_tulis = 5000 # RP5.000
+pensil = 4500 # Rp4.500
+total_buku_tulis = buku_tulis * 3
+total_pensil = pensil * 2
+total_harga = total_buku_tulis + total_pensil
+print("total_harga buku tulis dan pensil:", total_harga)
+total_sebelum_kena_pajak = 24000
+pajak = 0.10 * 24000
+print("pajak:", pajak)
+total_semua = total_sebelum_kena_pajak + pajak
+print("total belanja:", total_semua)
+print("="*30)
+
+
